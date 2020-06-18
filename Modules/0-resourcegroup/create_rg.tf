d96b70bbfb70e02b6d138ec3fea88c718d860183
Flodu31 <florent.appointaire@gmail.com>
2020-06-18 09:44:53 +0200

    Adding RG module

---
+++ Modules/0-resourcegroup/create_rg.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "rg_name" {
+  name                = "${var.rg_name}"
+  location            = "${var.location}"
+}
---
+++ Modules/0-resourcegroup/variables.tf
@@ -0,0 +1,6 @@
+variable "location" {
+  description = "Location where to deploy resources"
+}
+variable "rg_name" {
+  description = "Name of the Resource Group that will be deployed"
+}
