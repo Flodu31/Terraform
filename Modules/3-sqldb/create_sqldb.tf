0b33adc6e01062c74443bb410d3dfb6a3140a6e3
Flodu31 <florent.appointaire@gmail.com>
2020-06-19 07:52:25 +0200

    Update create_sqldb.tf

--- Modules/3-sqldb/create_sqldb.tf
+++ Modules/3-sqldb/create_sqldb.tf
@@ -3,9 +3,9 @@ data "azurerm_sql_server" "sqldemo" {
   resource_group_name          = "SQL"
 }
 
-resource "azurerm_sql_database" "example" {
+resource "azurerm_sql_database" "deploy_db" {
   name                = var.db_name
   resource_group_name = var.rg_name
   location            = var.location
-  server_name         = data.sqldemo.name
+  server_name         = data.azurerm_sql_server.sqldemo.name
 }
