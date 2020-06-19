9c2169e2d8030695b700a0ac50fe6f2f4e15bd1c
Flodu31 <florent.appointaire@gmail.com>
2020-06-19 08:00:43 +0200

    update with terraform >0.12

--- Modules/0-resourcegroup/create_rg.tf
+++ Modules/0-resourcegroup/create_rg.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "rg_name" {
-  name = "${var.rg_name}"
-  location = "${var.location}"
+  name = var.rg_name
+  location = var.location
 }
--- Modules/2-storage/create_storageaccount.tf
+++ Modules/2-storage/create_storageaccount.tf
@@ -4,8 +4,8 @@ resource "random_id" "storage_account" {
 
 resource "azurerm_storage_account" "storageaccount" {
   name                     = "${lower(random_id.storage_account.hex)}" // hex - The generated id presented in padded hexadecimal digits. This result will always be twice as long as the requested byte length.
-  resource_group_name      = "${var.rg_name}"
-  location                 = "${var.location}"
+  resource_group_name      = var.rg_name
+  location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
