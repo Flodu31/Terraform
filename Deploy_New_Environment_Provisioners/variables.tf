f67edf16e0c987f2d06f28bc582969f516072d0d
prakashk0301 <34745120+prakashk0301@users.noreply.github.com>
2020-06-30 16:44:24 +0530

    Update variables.tf

--- Deploy_New_Environment_Provisioners/variables.tf
+++ Deploy_New_Environment_Provisioners/variables.tf
@@ -1,8 +1,8 @@
 variable "location" {
-  default = "westeurope"
+  default = "West US"
 }
 variable "admin_username" {
-  default = "testadmin"
+  default = "AzureUser"
 } 
 variable "rg_keyvault" {
   default = "Keyvault"
@@ -14,23 +14,17 @@ variable "computer_name_Windows" {
   default = "WS01"
 }
 variable "rg_network" {
-  default = "Network-CloudyJourney-RG"
+  default = "spcclient-RG"
 }
 
 variable "vmsize" {
   description = "VM Size for the Production Environment"
-  type        = "map"
+  type        = "string"
+  default = "Standard_D2s_v3"
 
-      default = {
-        small         =   "Standard_DS1_v2"
-        medium        =   "Standard_D2s_v3"
-        large         =   "Standard_D4s_v3"
-        extralarge    =   "Standard_D8s_v3"
-      }
-}
 
 variable "os_ms" {
-  description = "Operating System for Database (MSSQL) on the Production Environment"
+  description = "Operating System for dev Environment"
   type        = "map"
 
       default = {
